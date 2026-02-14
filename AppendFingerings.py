53d22ac5ecfd6736131d4944c5cef7d8767d4e52
Sean Peacock <s.peacock@verizon.net>
2026-02-14 18:51:23 -0500

    Fixed AppendFingerings.py

--- AppendFingerings.py
+++ AppendFingerings.py
@@ -27,23 +27,17 @@ from music21 import articulations
 def addFingeringToPart(part, fingering):
     notes = part.recurse().notes
     num_notes = len(notes)
-    i = len(fingering) - 1
-    j = 1
+    j = 0
 
-    while i >= 0:
-        cur_note = notes[num_notes - j]
-
-        if cur_note.tie and cur_note.tie.type in ('continue', 'stop'):
+    for i in range(num_notes):
+        note = notes[i]
+        if note.tie and note.tie.type in ('continue', 'stop'):
             j += 1
             continue
 
-        if cur_note.isNote:
-            cur_note.articulations.append(articulations.Fingering(fingering[i][0]))
-            j += 1
-            i -= 1
+        if note.isNote:
+            note.articulations.append(articulations.Fingering(fingering[i - j][0]))
 
-        elif cur_note.isChord:
-            for k in range (0, len(cur_note.pitches)):
-                cur_note.articulations.append(articulations.Fingering(fingering[i][k]))
-            i -= 1
-            j += 1
+        elif note.isChord:
+            for k in range(0, len(note.pitches)):
+                note.articulations.append(articulations.Fingering(fingering[i - j][k]))
