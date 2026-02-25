f9a259cb3cdc8360360011a23e47dd235a51c011
Sean Peacock <s.peacock@verizon.net>
2026-02-24 20:39:20 -0500

    Fixes

--- AppendFingerings.py
+++ AppendFingerings.py
@@ -39,41 +39,43 @@ def addFingeringToPart(part, fingering):
         chord = chords[i]
         note = notes[i+n]
 
-        if note.tie and note.tie.type in ('continue', 'stop'):
-            if note.isChord:
-                if note.pitches == notes[i - 1].pitches:
+        if chord.tie and chord.tie.type in ('continue', 'stop'):
+            if chord.isChord:
+                if chord.pitches == chords[i - 1].pitches:
                     j += 1
                     continue
-            else:
-                if note.pitch in notes[i - 1].pitches:
+                elif len(chord.pitches) == 1 and chord.pitches[0] in chords[i - 1].pitches:
                     j += 1
                     continue
 
         f = fingering[i - j]
 
-        if chord.tie and chord.tie.type in ('continue', 'stop'):
-            indexes = [chord.pitches.index(pitch) for pitch in note.pitches]
+        if len(note.pitches) != len(chord.pitches):
+            indexes = []
+            for k in range(0, len(chord.pitches)):
+                checkNote = notes[i+n+k]
+                if len(checkNote.pitches) + k > len(chord.pitches):
+                    break
+
+                if checkNote.isChord:
+                    for l in range(0, len(checkNote.pitches)):
+                        if checkNote.pitches[l] in chord.pitches:
+                            indexes.append(chord.pitches.index(checkNote.pitches[l]))
+                    k += len(checkNote.pitches) - 1
+
+                else:
+                    if checkNote.pitches[0] in chord.pitches:
+                        indexes.append(chord.pitches.index(checkNote.pitches[0]))
             f = [f[index] for index in indexes]
 
-
-        #if note.isNote:
-        #    note.articulations.append(articulations.Fingering(f[0]))
-
-        #elif note.isChord:
-        f.reverse()
         count = 0
-        chordFingering = []
         for k in range(0, len(f)):
             if note.isChord:
-                if len(chordFingering) == 0:
-                    chordFingering = f[k:k+len(note.pitches)]
-                    chordFingering.reverse()
 
-                note.articulations.append(articulations.Fingering(chordFingering[count]))
+                note.articulations.append(articulations.Fingering(f[k]))
                 count += 1
                 if count == len(note.pitches):
                     count = 0
-                    chordFingering = []
                     n += 1
                     if i + n < num_notes:
                         note = notes[i + n]
